32a63dafbde5754a8bd6d433f72262acb3d67dc0
User <tooxa85@gmail.com>
2025-03-27 22:52:23 +0300

    Домашняя работа №26

--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,38 @@
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, DeleteView, CreateView
 
+from catalog.forms import ProductForm, ContactForm
 from catalog.models import Product, Contact
 
 
 class ProductListView(ListView):
     model = Product
+    form_class = ProductForm
     template_name = 'products/product_list.html'
 
 
 class ProductDetailView(DetailView):
     model = Product
+    form_class = ProductForm
     template_name = 'products/product_detail.html'
 
 
 class ContactsView(TemplateView):
     model = Contact
-    fields = ['name', 'phone', 'message']
+    form_class = ContactForm
     template_name = 'products/contacts.html'
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'products/product_delete.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/product_form.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+
