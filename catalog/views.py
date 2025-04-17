fba46751cd47120db5d02292988191e0e0c39a8d
User <tooxa85@gmail.com>
2025-04-17 17:35:31 +0300

    Домашняя работа №29

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,12 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, DeleteView, CreateView, UpdateView
 
-from catalog.forms import ProductForm, ContactForm, ProductModeratorForm
+from catalog.forms import ProductForm, ContactForm, ProductModeratorForm, Category
 from catalog.models import Product, Contact
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
+from django.core.cache import cache
+from .services import get_products_by_category, get_products_from_cache
 
 
 class ProductListView(ListView):
@@ -13,10 +15,22 @@ class ProductListView(ListView):
     template_name = 'products/product_list.html'
 
 
+    def get_queryset(self):
+        return get_products_from_cache()
+
+
 class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
     form_class = ProductForm
     template_name = 'products/product_detail.html'
+
+
+    def get_queryset(self):
+        queryset = cache.get('category_queryset')
+        if not queryset:
+            queryset = super().get_queryset()
+            cache.set('category_queryset', queryset, 60 * 15)
+        return queryset
 
 
 class ContactsView(TemplateView):
@@ -68,3 +82,11 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         if self.request.user.has_perm("catalog.can_unpublish_product"):
             return ProductModeratorForm
         raise PermissionDenied
+
+
+class ProductsByCategoryView(ListView):
+    model = Category
+    # template_name = 'products/category_list.html'
+    def get_queryset(self):
+        category_id = self.kwargs.get('pk')
+        return get_products_by_category(category_id=category_id)
